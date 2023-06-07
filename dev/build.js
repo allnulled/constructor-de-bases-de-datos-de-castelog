e19653a51110cfdeddab69a463b10f751a3cdc43
allnulled <todosanulados@gmail.com>
2023-06-07 10:34:01 +0200

    arregladas algunas cosas

--- dev/build.js
+++ dev/build.js
@@ -1,8 +1,15 @@
+const child_process = require("child_process");
 const fs = require("fs");
 const path = require("path");
 
+
+Copiar_todo_en_docs: {
+    child_process.execSync("cp -R ./src/www/* ./docs", {
+        cwd: path.resolve(__dirname, "..")
+    });
+}
 Copiar_index_en_docs: {
-    const fichero_origen = path.resolve(__dirname, "..", "src/www/app-por-defecto.0.0.1.html");
+    const fichero_origen = path.resolve(__dirname, "..", "src/www/app.ultima.html");
     const fichero_destino = path.resolve(__dirname, "..", "docs/index.html");
     fs.copyFileSync(fichero_origen, fichero_destino);
 }
---
+++ docs/js/download_file.js
@@ -0,0 +1,9 @@
+window.download_file_by_source = function(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+};
